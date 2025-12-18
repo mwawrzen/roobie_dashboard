46249b275046a226d7942c24c7e9a10f581c5886
mwawrzen <w.w@op.pl>
2025-12-18 23:30:31 +0100

    feat: create login middleware

---
+++ app/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware( request: NextRequest ) {
+  const token= request.cookies.get( "auth" );
+
+  if( !token&& request.nextUrl.pathname.startsWith( "/dashboard" ))
+    return NextResponse.redirect( new URL( "/login", request.url ));
+
+  if( token&& request.nextUrl.pathname=== "/login" )
+    return NextResponse.redirect( new URL( "/dashboard", request.url ));
+
+  return NextResponse.next();
+};
+
+export const config= {
+  matcher: [ "/dashboard/:path*", "/login" ]
+};
