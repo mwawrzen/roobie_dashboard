f4d85fa87acbf49240f5355dcf06a53a17a982bc
mwawrzen <w.w@op.pl>
2025-12-19 15:49:19 +0100

    feat: create global not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center gap-4">
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        404 Not Found
+      </h2>
+      <Link href="/dashboard">
+        <Button variant="outline">Return to Dashboard</Button>
+      </Link>
+    </main>
+  );
+};
