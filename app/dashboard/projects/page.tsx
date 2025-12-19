0af8258b461f70e152a9ea33f5b4a768897e41c2
mwawrzen <w.w@op.pl>
2025-12-19 17:18:16 +0100

    feat: remove unused lines

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,6 +1,3 @@
-import { DataTable } from "@/components/data-table";
-
-import data from "../data.json";
 import { Project, projectService } from "@/services/project.service";
 import { ProjectsDataTable } from "@/components/projects-data-table";
 
@@ -10,13 +7,4 @@ export default async function ProjectsPage() {
   return (
     <ProjectsDataTable data={ projects } />
   );
-
-  // return (
-  //   <main>
-  //     <h1>Your projects</h1>
-  //     <ul>
-  //       { projects.map(( p: any )=> <li key={p.id}>{p.name}</li> )}
-  //     </ul>
-  //   </main>
-  // );
 };
