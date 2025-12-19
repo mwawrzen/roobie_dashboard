1c3babf56456815c68aa5e5b8a516192c25681d8
mwawrzen <w.w@op.pl>
2025-12-19 14:35:45 +0100

    feat: create project page

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,26 +1,22 @@
-import { cookies } from "next/headers";
+import { DataTable } from "@/components/data-table";
+
+import data from "../data.json";
+import { Project, projectService } from "@/services/project.service";
+import { ProjectsDataTable } from "@/components/projects-data-table";
 
 export default async function ProjectsPage() {
-  const cookieStore= await cookies();
-  const token= cookieStore.get( "auth" )?.value;
-
-  const res= await fetch( "http://localhost:3001/api/v1/projects", {
-    headers: {
-      "Cookie": `auth=${ token }`
-    }
-  });
-
-  if( res.status=== 401 ) return <p>You must be logged in!</p>;
-  if( res.status=== 403 ) return <p>You have no privilege to see projects list</p>;
-
-  const projects= await res.json();
+  const projects: Project[]= await projectService.getAll();
 
   return (
-    <main>
-      <h1>Your projects</h1>
-      <ul>
-        { projects.map(( p: any )=> <li key={p.id}>{p.name}</li> )}
-      </ul>
-    </main>
+    <ProjectsDataTable data={ projects } />
   );
+
+  // return (
+  //   <main>
+  //     <h1>Your projects</h1>
+  //     <ul>
+  //       { projects.map(( p: any )=> <li key={p.id}>{p.name}</li> )}
+  //     </ul>
+  //   </main>
+  // );
 };
