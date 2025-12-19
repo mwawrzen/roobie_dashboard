288334b7682baea69d7da5641d3e66571f69fdf8
mwawrzen <w.w@op.pl>
2025-12-19 23:32:37 +0100

    feat: create projects page header with create button

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,10 +1,20 @@
 import { Project, projectService } from "@/services/project.service";
 import { ProjectsDataTable } from "@/components/projects-data-table";
+import { Button } from "@/components/ui/button";
+import { IconPlus } from "@tabler/icons-react";
 
 export default async function ProjectsPage() {
   const projects: Project[]= await projectService.getAll();
 
   return (
-    <ProjectsDataTable data={ projects } />
+    <main>
+      <header className="mb-6">
+        <Button variant="secondary" size="default">
+          <IconPlus />
+          Create
+        </Button>
+      </header>
+      <ProjectsDataTable data={ projects } />
+    </main>
   );
 };
