fbd670af468b3853f3662e0be92f6e4d828dc3e0
mwawrzen <w.w@op.pl>
2025-12-20 01:31:42 +0100

    feat: create action for adding variable

---
+++ app/dashboard/projects/[id]/actions.ts
@@ -0,0 +1,21 @@
+import { projectService } from "@/services/project.service";
+import { revalidatePath } from "next/cache";
+
+export async function addVariableAction(
+  projectId: number,
+  formData: FormData
+) {
+  const key= formData.get( "key" ) as string;
+  const value= formData.get( "value" ) as string;
+
+  if( !key|| !value )
+    return { error: "Key and value required" };
+
+  try {
+    await projectService.addVariable( projectId, key, value );
+    revalidatePath( `/dashboard/projects/${ projectId }` );
+    return { success: true };
+  } catch( e ) {
+    return { error: "Error while adding variable" };
+  }
+};
