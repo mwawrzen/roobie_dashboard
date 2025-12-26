b53aa57cc796819ffde4c8fc2cfababe549272e0
mwawrzen <w.w@op.pl>
2025-12-26 14:14:08 +0100

    feat: create project page

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -4,16 +4,11 @@ import { Button } from "@/components/ui/button";
 import { IconPlus } from "@tabler/icons-react";
 
 export default async function ProjectsPage() {
+
   const projects: Project[]= await projectService.getAll();
 
   return (
     <main>
-      <header className="mb-6">
-        <Button variant="secondary" size="default">
-          <IconPlus />
-          Create
-        </Button>
-      </header>
       <ProjectsDataTable data={ projects } />
     </main>
   );
